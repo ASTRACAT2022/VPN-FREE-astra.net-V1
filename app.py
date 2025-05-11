cce4faab37175048d166cb760c62041c1c401f37
ASTRACAT2022 <107486261+ASTRACAT2022@users.noreply.github.com>
2025-05-11 15:15:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ async def serve_configs(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
